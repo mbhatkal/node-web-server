877f3aa6f0ce558e8c814621e8dc3efb92b156ef
mbhatkal <bhatkal.mahesh@gmail.com>
2020-04-05 18:42:44 +0530

    app.js with console log in pages

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(express.static('public')); // all html files in static directory
 app.set('view_engine','hbs');
 app.listen(port); // wait for a connection from a browser
 app.get('/',(req,res) =>{ // respond to the get root request
+  console.log('Inside Home page');
   res.render('index.hbs',{
     pageHeading:'Home Page',
     href1:'/',
@@ -25,6 +26,7 @@ app.get('/',(req,res) =>{ // respond to the get root request
   });
 });
 app.get('/about',(req,res) =>{ // respond to the get root request
+  console.log('Inside About page');
   res.render('index.hbs',{
     pageHeading:'About Page',
     href1:'/',
@@ -36,6 +38,7 @@ app.get('/about',(req,res) =>{ // respond to the get root request
   });
 });
 app.get('/help',(req,res) =>{ // respond to the get root request
+  console.log('Inside help page');
   res.render('index.hbs',{
     pageHeading:'Help Page',
     href1:'/',
@@ -47,10 +50,12 @@ app.get('/help',(req,res) =>{ // respond to the get root request
   });
 });
 app.get('/customer',(req,res) =>{ // respond to the get root request
+console.log('Inside customer page');
 res.send(customer);
 });
 // for all the routes which are not defined
 app.use(function (req, res, next) {
+  console.log('Inside Unknown page');
   res.status(404).send("Sorry can't find that!")
 })
 console.log('Server up and Running');
